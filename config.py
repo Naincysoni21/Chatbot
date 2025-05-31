774404bee679ec6a5d6627bf7299ec1e17be00f2
RADHA MUSIC <vnvn30352@gmail.com>
2025-05-31 10:17:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "20031520"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "a9bf896776df81c44f5b17ea535fb748"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1679112664"))
-SUPPORT_GRP = "+cXIPgHSuJnxiNjU1"
-UPDATE_CHNL = "ai_image_junction"
-OWNER_USERNAME = "OfficialDurgesh"
+OWNER_ID = int(getenv("OWNER_ID", "5923034665"))
+SUPPORT_GRP = "RADHA_MUSIC_SUPPORT"
+UPDATE_CHNL = "NAINCY_UPDATES"
+OWNER_USERNAME = "Vishnusoni14"
